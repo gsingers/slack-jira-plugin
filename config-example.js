509b39d311f35c3f403bef1e69304065ee201d8b
gsingers <gsingers@apache.org>
2015-01-12 19:08:32 -0500

    Multiple repo support

--- config-example.js
+++ config-example.js
@@ -5,9 +5,10 @@ var config = {
     token: 'XXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXXXXXX-XXXXXX',
     jira_urls: {
       "APACHE": "https://issues.apache.org/jira/browse/",
-      "REPLACE": "https://my.path.to.jira/browse/"
+      "REPLACE": "https://my.path.to.jira/browse/",
+      "DEFAULT": "https://my.path.to.jira/browse/"
     },
-    pattern: /((GRANT))-\d+/g,
+    pattern: /((GRANT|BOB))-\d+/g//NOTE this assumes all JIRA issues are like: PROJECT_NAME-1234
 };
 
 //DO NOT EDIT BELOW HERE
